2b07bd5e8c37bce910afd6c93b24cbeba04847d6
Mridul Malviya <mridul.malviya@gmail.com>
2018-03-31 14:49:34 +0530

    Initial Changes

--- Test2/Test2.Android/MainActivity.cs
+++ Test2/Test2.Android/MainActivity.cs
@@ -12,12 +12,14 @@ namespace Test2.Droid
     [Activity(Label = "Test2", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        internal static MainActivity Instance { get; private set; }
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
+            Instance = this;
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
